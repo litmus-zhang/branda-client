17878c093c90f8d3f4f3b65b8597b0866c2adf0d
Jude Tochy <tochyokoye@gmail.com>
2024-06-19 13:18:52 +0100

    worked on UI

--- src/components/new/new.tsx
+++ src/components/new/new.tsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from "react";
 import { MultiStepFormStyle, StageStyle, VaryingStepStyle } from "./style";
 import { motion } from "framer-motion";
+import ReactConfetti from "react-confetti";
+import { CopyToClipboard } from "@/utils/copyText";
 
 export const Dropdown = () => {
   return <>I am dropdown</>;
@@ -8,6 +10,8 @@ export const Dropdown = () => {
 
 export const MultiStepForm = () => {
   const [trackStages, setTrackStages] = useState(1);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isCopy, setIsCopy] = useState(false);
   const handleSelection = (step: string) => {
     if (step === "name") {
       setTrackStages(trackStages + 1);
@@ -22,12 +26,18 @@ export const MultiStepForm = () => {
     } else if (step === "tag") {
       setTrackStages(trackStages + 1);
     } else if (step === "pattern") {
-      handleScroll(50);
+      handleScroll(100);
       setTrackStages(trackStages + 1);
     } else if (step === "photo") {
+      setIsSuccess(true);
+      setTrackStages(trackStages + 1);
+      setTimeout(() => {
+        setIsSuccess(false);
+      }, 7000);
+    } else if (step === "copy") {
+      CopyToClipboard("link");
+      setIsCopy(true);
     }
-    // else if (step === "pitchdeck") {
-    // }
   };
   // slider when the step gets to the middle
   const containerRef = useRef<HTMLDivElement>(null);
@@ -41,116 +51,130 @@ export const MultiStepForm = () => {
     }
   };
   return (
-    <MultiStepFormStyle>
-      <div className="stage-head" ref={containerRef}>
-        {[1, 2, 3, 4, 5, 6, 7].map((ele, index) => (
-          <StageStyle key={index} $isSelected={ele <= trackStages}>
-            <motion.div className="circle" layout>
-              <p>{ele}</p>
-            </motion.div>
-            {index !== 6 && <div className="dash"></div>}
-          </StageStyle>
-        ))}
-      </div>
-      {trackStages === 1 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Name</h3>
-            <p>Choose your preferred brand logo</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("name")}>
-              Select brand name
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 2 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Logo</h3>
-            <p>Choose your preferred brand logo</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("logo")}>
-              Select brand logo
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 3 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Color</h3>
-            <p>Choose your preferred brand color</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("color")}>
-              Select brand color
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 4 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Typography</h3>
-            <p>Choose your preferred brand font type</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("font")}>
-              Select brand font
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 5 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Tag line/Messaging</h3>
-            <p>Choose your preferred brand tagline</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("tag")}>
-              Select brand tag line
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 6 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand Pattern</h3>
-            <p>Choose your preferred brand pattern</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("pattern")}>
-              Select brand pattern
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {trackStages === 7 && (
-        <VaryingStepStyle>
-          <div className="top">
-            <h3>Brand photography</h3>
-            <p>Choose your preferred brand photography</p>
-          </div>
-          <div className="otions"></div>
-          <div className="btn">
-            <button type="submit" onClick={() => handleSelection("photo")}>
-              Select brand photo
-            </button>
-          </div>
-        </VaryingStepStyle>
-      )}
-      {/* {trackStages === 8 && (
+    <>
+      <MultiStepFormStyle>
+        <div className="stage-head" ref={containerRef}>
+          {[1, 2, 3, 4, 5, 6, 7].map((ele, index) => (
+            <StageStyle key={index} $isSelected={ele <= trackStages}>
+              <motion.div className="circle" layout>
+                <p>{ele}</p>
+              </motion.div>
+              {index !== 6 && <div className="dash"></div>}
+            </StageStyle>
+          ))}
+        </div>
+        {trackStages === 1 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Name</h3>
+              <p>Choose your preferred brand logo</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("name")}>
+                Select brand name
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 2 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Logo</h3>
+              <p>Choose your preferred brand logo</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("logo")}>
+                Select brand logo
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 3 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Color</h3>
+              <p>Choose your preferred brand color</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("color")}>
+                Select brand color
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 4 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Typography</h3>
+              <p>Choose your preferred brand font type</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("font")}>
+                Select brand font
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 5 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Tag line/Messaging</h3>
+              <p>Choose your preferred brand tagline</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("tag")}>
+                Select brand tag line
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 6 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand Pattern</h3>
+              <p>Choose your preferred brand pattern</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("pattern")}>
+                Select brand pattern
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 7 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Brand photography</h3>
+              <p>Choose your preferred brand photography</p>
+            </div>
+            <div className="otions"></div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("photo")}>
+                Select brand photo
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {trackStages === 8 && (
+          <VaryingStepStyle>
+            <div className="top">
+              <h3>Congratulations!</h3>
+              <p>Your brand identity have been created!</p>
+            </div>
+            <div className="btn">
+              <button type="submit" onClick={() => handleSelection("copy")}>
+                {isCopy ? "Copied Link" : "Copy Link"}
+              </button>
+            </div>
+          </VaryingStepStyle>
+        )}
+        {/* {trackStages === 8 && (
           <VaryingStepStyle>
             <div className="top">
               <h3>Brand Pitch Deck</h3>
@@ -164,6 +188,10 @@ export const MultiStepForm = () => {
             </div>
           </VaryingStepStyle>
         )} */}
-    </MultiStepFormStyle>
+      </MultiStepFormStyle>
+      {isSuccess && (
+        <ReactConfetti width={window.innerWidth} height={window.innerHeight} />
+      )}
+    </>
   );
 };
